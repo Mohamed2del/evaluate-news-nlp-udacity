4bf10c696b0f2f7c22f1612c42e17d65a2eb8867
Mohamed Adel <Mohamedadelmohamed997@gmail.com>
2021-03-26 21:32:04 +0200

    fix from feedback

--- src/client/index.js
+++ src/client/index.js
@@ -1,11 +1,14 @@
 import { handleSubmit } from './js/formHandler';
 import { checkForURL } from './js/urlcheck';
-
+require('babel-core/register');
+require('babel-polyfill');
 import './styles/resets.scss';
 import './styles/base.scss';
 import './styles/footer.scss';
 import './styles/form.scss';
 import './styles/header.scss';
+
+('use strict');
 
 // Gets a reference to the form element
 var form = document.getElementById('form');
