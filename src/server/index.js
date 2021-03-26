b7c2b001c94ffc0aa2e34efb7b0fa2b824168353
Mohamed Adel <Mohamedadelmohamed997@gmail.com>
2021-03-26 15:39:10 +0200

    dotenv

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,28 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
 
-const app = express()
+const app = express();
 
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 
-console.log(__dirname)
+console.log(__dirname);
+
+// You could call it aylienapi, or anything else
+var textapi = new aylien({
+  application_key: '7eefda69a6582c70ab4af58700120961',
+});
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
+  // res.sendFile('dist/index.html')
+  res.sendFile(path.resolve('src/client/views/index.html'));
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+  console.log('Example app listening on port 8080!');
+});
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+  res.send(mockAPIResponse);
+});
